feab144f833d59055dc9301c4562265a6d54ed1a
Ana Oliveira <oliveirabrancoana@gmail.com>
2022-05-05 10:45:23 -0300

    Primeiro

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿namespace T04_22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string nome, cargo;
+            decimal salario;
+            int tempo;
+
+            Console.Write("Nome do funcionario: ");
+            nome = Console.ReadLine();
+            Console.Write($"Digite qual será o cargo de {nome} ? ");
+            cargo = Console.ReadLine();
+            Console.Write($"Qual salário inicial de {nome} como {cargo} ? ");
+            salario = decimal.Parse(Console.ReadLine());
+            Console.Write($"Por quantos meses voçê quer contratar {nome} ? ");
+            tempo = int.Parse(Console.ReadLine());
+
+            if (tempo >= 7)
+            {
+                Console.WriteLine($"{nome} será efetivado(a) como {cargo} com o salário no valor de {salario} ");
+                Console.WriteLine($" Parabéms voçê faz parte da nossa equipe! ");
+                Console.WriteLine();
+            
+            }
+            else
+            {
+                Console.WriteLine($"{nome} será membro honorário como {cargo},  com o salario {salario}, pro {tempo} meses!");
+                Console.WriteLine();
+            }
+
+
+
+        }
+    }
+}
